874de9559eb97165d780a7c9de7ef8aeee747c5a
Ben Reese <reeseb@gmail.com>
2013-03-26 18:54:27 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thebeerstate::Application.config.secret_token = 'fd90d60e1d0c89e065bcd9968fe86371a9aa1b1e81b873de777e415b61a9a102eb7e5add7f7b7421b3e33e1b62d6bebbddea0bdc08f13797d39b1f976f5bf8d3'
