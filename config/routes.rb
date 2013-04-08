1d7049ddc7cf8017ae82287fda292442ac982759
Ben Reese <reeseb@gmail.com>
2013-04-07 23:05:15 -0600

    Fixed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Thebeerstate::Application.routes.draw do
   devise_for :users
 
   get 'about' => 'pages#about'
-  root :to => 'Pages#home'
+  root :to => 'pins#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
